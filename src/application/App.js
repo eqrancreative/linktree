743cbc89a1b5176601a2b52b28ec0b22a2d8118b
Matheus Barcelos <barcelosm1996@gmail.com>
2023-01-19 18:20:54 -0300

    Clock icon alternation and margin-right to 4px implemented

--- src/application/App.js
+++ src/application/App.js
@@ -1,54 +1,67 @@
-import React from "react";
+import React, {useState} from "react";
 import Button from "./components/Button";
-import {logo, whatsapp, email, website, clock} from "../img/__img__";
+import * as img from "../img/__img__";
 import "../css/style.css";
 
-class App extends React.Component {
-    render() {
-        return (
-            <main>
-                <div className={"container"}>
-                    <div className={"content"}>
-                        <img src={logo} alt={"JM Log"} className={"logo"}/>
-                        <h2 style={{ marginBottom: "4px" }}>
-                            João & Maria Closet
-                        </h2>
-                        <p className={"subtitle"}>
-                            Vestindo príncipes e princesas com qualidade e conforto.
-                        </p>
-                    </div>
-                    <div className={"content"}>
-                        <Button
-                            icon={whatsapp}
-                            description={"FALE CONOSCO"}
-                            enable={true}
-                            action={() => {
-                                window.open(`https://wa.me/558199171584`, "__blank")
-                            }}
-                        />
-                        <Button
-                            icon={email}
-                            description={"ENVIE UM EMAIL"}
-                            enable={true}
-                            action={() => {
-                                window.location.href = "mailto:mjm3xavier@gmail.com";
-                            }}
-                        />
-                        <Button
-                            icon={website}
-                            description={"ACESSE NOSSO SITE"}
-                            enable={false}/>
-                    </div>
-                    <div className={"content"}>
-                        <img src={clock} alt={"Ícone de relógio"} className={"clock-icon"}/>
-                        <p>
-                            Seg à Sex 8 às 18
-                        </p>
-                    </div>
-                </div>
-            </main>
-        )
+function App() {
+
+    const [day] = useState(new Date().getDay())
+    const [hour] = useState(new Date().getHours())
+
+    const handleClock = () => {
+        return day < 6 && day > 1 && hour < 18 ?
+            img.clockOpen
+            :
+            img.clockClose
     }
+
+    return (
+        <main>
+            <div className={"container"}>
+                <div className={"content"}>
+                    <img src={img.logo} alt={"JM Log"} className={"logo"}/>
+                    <h2>
+                        João & Maria Closet
+                    </h2>
+                    <p className={"subtitle"}>
+                        Vestindo príncipes e princesas com qualidade e conforto.
+                    </p>
+                </div>
+                <div className={"content"}>
+                    <Button
+                        icon={img.whatsapp}
+                        description={"FALE CONOSCO"}
+                        enable={true}
+                        action={() => {
+                            window.open(`https://wa.me/558199171584`, "__blank")
+                        }}
+                    />
+                    <Button
+                        icon={img.email}
+                        description={"ENVIE UM EMAIL"}
+                        enable={true}
+                        action={() => {
+                            window.location.href = "mailto:mjm3xavier@gmail.com";
+                        }}
+                    />
+                    <Button
+                        icon={img.website}
+                        description={"ACESSE NOSSO SITE"}
+                        enable={false}/>
+                </div>
+                <div className={"content"}>
+                    <img
+                        src={handleClock()}
+                        alt={"Ícone de relógio"}
+                        className={"clock-icon"}
+                    />
+                    <p>
+                        Seg à Sex 8 às 18
+                    </p>
+                </div>
+            </div>
+        </main>
+    )
 }
 
 export default App;
--- src/img/__img__.js
+++ src/img/__img__.js
@@ -2,8 +2,9 @@ import logo from "../img/logo.svg";
 import whatsapp from "../img/whatsapp.svg";
 import email from "../img/email.svg";
 import website from "../img/website.svg";
-import clock from "../img/clock.svg";
+import clockClose from "./clock-close.svg";
+import clockOpen from "./clock-open.svg";
 
 export {
-    logo, whatsapp, email, website, clock
+    logo, whatsapp, email, website, clockClose, clockOpen
 }
